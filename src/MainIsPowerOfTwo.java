2cf4d9b12f68cabbfd4d39866d601e29faaae43d
Grigory Kislin <javawebinar@yandex.ru>
2016-05-24 13:32:05 +0300

    Initial check-in

---
+++ src/MainIsPowerOfTwo.java
@@ -0,0 +1,24 @@
+/**
+ * GKislin
+ * 24.05.2016
+ */
+public class MainIsPowerOfTwo {
+    public static void main(String[] args) {
+        print(-5);
+        print(0);
+        print(1);
+        print(2);
+        print(3);
+        print(17);
+        print(2048);
+        print(Integer.MAX_VALUE);
+    }
+
+    static boolean isPowerOfTwo(int value) {
+        return false;
+    }
+
+    static void print(int value) {
+        System.out.println(value + (isPowerOfTwo(value) ? " is power of 2" : " is not power of 2"));
+    }
+}
